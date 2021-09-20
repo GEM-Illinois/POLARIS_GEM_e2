e97535a7fec3f3f835ddcaa4e6ab8f18f9f9915f
Hsieh, Chiao <chsieh16@illinois.edu>
2021-09-20 11:51:46 -0500

    Fix LaneNet pipeline with new perspective transformation

--- polaris_gem_lane_detect_sim/gem_lanenet/nodes/lanenet_node.py
+++ polaris_gem_lane_detect_sim/gem_lanenet/nodes/lanenet_node.py
@@ -1,34 +1,43 @@
+#!/usr/bin/env python3
 from typing import Optional
 import sys
 
 import tensorflow
 
 import rospy
-from geometry_msgs.msg import Vector3Stamped
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
+from gem_lane_detect_msgs.msg import SimpleLaneStamped
 from gem_lanenet.lanenet_w_line_fit import LaneNetWLineFit
 
 
 class LaneNetLaneDetector:
     def __init__(self, nnet: LaneNetWLineFit,
                  pub_lane: rospy.Publisher,
-                 pub_mask_img: Optional[rospy.Publisher] = None) -> None:
+                 pub_annotated_img: Optional[rospy.Publisher] = None,
+                 ) -> None:
         self._nnet = nnet
         self._pub_lane = pub_lane
-        self._pub_mask_img = pub_mask_img
+        self._pub_annotated_img = pub_annotated_img
 
         self._bridge = CvBridge()
 
-    def img_callback(self, data):
+    def img_callback(self, data: Image):
         try:
             cv_image = self._bridge.imgmsg_to_cv2(data, "bgr8")
-            mask_image, offset, curvature = self._nnet.detect(cv_image)
+            yaw_err, offset, curvature, annotated_image = self._nnet.detect(cv_image)
 
-            if self._pub_mask_img:
-                mask_image_msg = self._bridge.cv2_to_imgmsg(mask_image, 'bgr8')
-                self._pub_mask_img.publish(mask_image_msg)
+            lane_stamped_msg = SimpleLaneStamped()
+            lane_stamped_msg.header.stamp = rospy.Time.now()
+            lane_stamped_msg.header.frame_id = "base_link"  # TODO
+            lane_stamped_msg.lane.yaw_err = yaw_err
+            lane_stamped_msg.lane.offset = offset
+            lane_stamped_msg.lane.curvature = curvature
+
+            if self._pub_annotated_img is not None:
+                annotated_image_msg = self._bridge.cv2_to_imgmsg(annotated_image, '8UC3')
+                self._pub_annotated_img.publish(annotated_image_msg)
 
         except CvBridgeError as e:
             rospy.logerr(e)
@@ -42,19 +51,16 @@ def main(argv) -> None:
     config_path = rospy.get_param("~config_path")
     weights_path = rospy.get_param("~weights_path")
     use_gpu = rospy.get_param("~use_gpu", True)
-    debug = rospy.get_param("~debug", True)
+    debug = rospy.get_param("~debug", False)
 
-    nnet = LaneNetWLineFit(config_path, weights_path, use_gpu)
+    nnet = LaneNetWLineFit(config_path, weights_path, use_gpu, debug=debug)
 
-    rospy.sleep(0)  # Wait for first simulated time message
-
-    pub_lane = rospy.Publisher("estimated_lane", Vector3Stamped, queue_size=1)
+    pub_lane = rospy.Publisher("estimated_lane", SimpleLaneStamped, queue_size=1)
     if debug:
-        pub_mask_image = rospy.Publisher("mask_image", Image, queue_size=1)
+        pub_annotated_image = rospy.Publisher("annotated_image", Image, queue_size=1)
+        lanenet_detector = LaneNetLaneDetector(nnet, pub_lane, pub_annotated_image)
     else:
-        pub_mask_image = None
-
-    lanenet_detector = LaneNetLaneDetector(nnet, pub_lane, pub_mask_image)
+        lanenet_detector = LaneNetLaneDetector(nnet, pub_lane)
 
     _ = rospy.Subscriber(
             "image_raw",
@@ -63,6 +69,8 @@ def main(argv) -> None:
         rospy.spin()
     except KeyboardInterrupt:
         rospy.loginfo("Shutting down LaneNet node")
+    finally:
+        nnet.close()
 
 
 if __name__ == '__main__':
--- polaris_gem_lane_detect_sim/gem_lanenet/src/gem_lanenet/lanenet_w_line_fit.py
+++ polaris_gem_lane_detect_sim/gem_lanenet/src/gem_lanenet/lanenet_w_line_fit.py
@@ -1,14 +1,202 @@
+from typing import Dict, Tuple, Optional
+
 import cv2
 import numpy as np
+from numpy.polynomial.polynomial import Polynomial
+import rospy
 import tensorflow as tf
-from lanenet.lanenet_model import lanenet
+from lanenet.lanenet_model import lanenet, lanenet_postprocess
 from lanenet.parse_config_utils import Config
-from gem_lanenet.line_fit import Line, line_fit_pipeline
+from gem_lanenet.line_fit import Line
+
+WARPED_IMG_W = 16*61  # pixels
+WARPED_IMG_H = 9*61  # pixels
+WARPED_IMG_SIZE = (WARPED_IMG_W, WARPED_IMG_H)
+METER_PER_PIXEL = 0.03 # each pixel is about 0.03 m in the warped image
+
+SRC_DST_MAP = {
+    (412, 329): (426, 60),
+    (401, 338): (426, 121),
+    (386, 351): (426, 182),
+    (364, 370): (426, 243),
+    (329, 400): (426, 304),
+    (263, 457): (426, 365),
+    (477, 329): (487, 60),
+    (477, 338): (487, 121),
+    (477, 351): (487, 182),
+    (476, 370): (487, 243),
+    (474, 400): (487, 304),
+    (472, 457): (487, 365),
+    (543, 329): (548, 60),
+    (553, 338): (548, 121),
+    (567, 351): (548, 182),
+    (588, 370): (548, 243),
+    (620, 400): (548, 304),
+    (681, 457): (548, 365),
+    (609, 329): (609, 60),
+    (630, 338): (609, 121),
+    (658, 351): (609, 182),
+    (699, 370): (609, 243),
+    (766, 400): (609, 304),
+    (891, 457): (609, 365),
+    (675, 329): (670, 60),
+    (706, 338): (670, 121),
+    (749, 351): (670, 182),
+    (811, 370): (670, 243),
+    (912, 400): (670, 304),
+    (1100, 457): (670, 365)}
+
+
+def get_perspective_transform(src_dst_map: Dict[Tuple[int, int], Tuple[int, int]]) \
+        -> Tuple[np.ndarray, np.ndarray]:
+    """ Compute perspective transformation matrices for converting images to
+        bird's eye view using homography.
+
+    Parameters
+    ----------
+    src_dst_map
+        A dictionary mapping a source pixel in source image to a destination
+        pixel in warped image
+
+    Returns
+    -------
+    matrix, matrix_inv
+        Perspective transformation matrix and its inverse
+    """
+    src_pts, dst_pts = list(zip(*src_dst_map.items()))
+    src_pts_arr = np.float32(src_pts)
+    dst_pts_arr = np.float32(dst_pts)
+    matrix, mask = cv2.findHomography(src_pts_arr, dst_pts_arr, cv2.RANSAC, 5.0)
+    matrix_inv, mask_inv = cv2.findHomography(dst_pts_arr, src_pts_arr, cv2.RANSAC, 5.0)
+    return matrix, matrix_inv
+
+
+def _find_pixels_in_window(pixel_arr: np.ndarray,
+                           curr_pixel: Tuple[int, int],
+                           img_size: Tuple[int, int],
+                           window_size: Tuple[int, int]):
+    u_curr, v_curr = curr_pixel
+    img_w, img_h = img_size
+    if not(0 <= u_curr < img_w and 0 <= v_curr < img_h):
+        raise ValueError("Given curr_pixel %s is not in image size %s"
+                         % (curr_pixel, img_size))
+    window_w, window_h = window_size
+
+    # Ensure within source image bound
+    window_u_lo, window_u_hi = u_curr, min(u_curr + window_w, img_w)
+    window_v_lo, window_v_hi = v_curr, min(v_curr + window_h, img_h)
+
+    pixel_filter = \
+        (window_u_lo <= pixel_arr[:, 0]) & (pixel_arr[:, 0] < window_u_hi) \
+        & (window_v_lo <= pixel_arr[:, 1]) & (pixel_arr[:, 1] < window_v_hi)
+    return pixel_arr[pixel_filter]
+
+
+def _find_lane_edge_pixels(nonzero_pixels: np.ndarray,
+                           base_vertical_line: int,
+                           img_size: Tuple[int, int],
+                           nwindows: int = 9,
+                           min_pixels: int = WARPED_IMG_W//24) \
+        -> np.ndarray:
+    """ Find the nonzero pixels for one lane
+
+    Parameters
+    ----------
+    nonzero_pixels
+        An array of pixels that have nonzero value in the source image
+    base_vertical_line
+        The vertical line to start searching for lane marking pixels
+    img_size
+        Size of the source image in (width, height)
+
+    Returns
+    -------
+    lane_pixel_arr
+        Array of pixels for the lane close to base_vertical_line
+
+    """
+    img_w, img_h = img_size
+    window_w, window_h = img_w // 12, img_h // nwindows  # FIXME tune the window width
+    u_base = base_vertical_line
+    if not(0 <= u_base < img_w):
+        raise ValueError("Given base vertical line %s is not in image size %s"
+                         % (u_base, img_size))
+
+    lane_pixels = []
+    u_curr = max(u_base - window_w//2, 0)
+    # Start from img_h to search from bottom-up
+    for v_curr in img_h - window_h*np.arange(1, nwindows+1, dtype=np.int):
+        good_pixel_arr = _find_pixels_in_window(
+            pixel_arr=nonzero_pixels,
+            curr_pixel=(u_curr, v_curr),
+            img_size=img_size,
+            window_size=(window_w, window_h))
+
+        if len(good_pixel_arr) > min_pixels:
+            new_u_base = int(np.mean(good_pixel_arr[:, 0]))
+            u_curr = max(new_u_base - window_w//2, 0)
+
+        # Append these pixels to the list
+        lane_pixels.extend(good_pixel_arr)
+
+    # Ensure the shape must be Nx2 even when N=0
+    lane_pixel_arr = np.array(lane_pixels, dtype=np.int32).reshape((-1, 2))
+    return lane_pixel_arr
+
+
+def _find_current_lane_pixels(binary_birdeye_img) -> Tuple[np.ndarray, np.ndarray]:
+    """ Find the pixels of the two lane edges for the current lane on the
+        binary bird's eye view image
+
+    Parameters
+    ----------
+    binary_birdeye_img: np.ndarray
+        Input binary bird's eye view image
+
+    Returns
+    -------
+    left_lane_pixels, right_lane_pixels
+        Two numpy arrays for left and right lane edges. Each represents an array of pixels.
+
+    Notes
+    -----
+    Pixel is a pair of (u, v) where 0<=u<img_w and 0<=v<img_h following
+    OpenCV convention.
+    Beware that u is column and v is row for Numpy arrays.
+
+    References
+    ----------
+    https://automaticaddison.com/the-ultimate-guide-to-real-time-lane-detection-using-opencv/
+    """
+    img_size = (binary_birdeye_img.shape[1], binary_birdeye_img.shape[0])
+
+    # Take a histogram of the bottom half of the image
+    bot_half_img = binary_birdeye_img[binary_birdeye_img.shape[0]//2:, :]
+    histogram = np.sum(bot_half_img, axis=0)
+
+    # Find the peak of the left and right halves of the histogram
+    # These will be the starting point for the left and right lines
+    midpoint = histogram.shape[0] // 2
+    left_u_base = np.argmax(histogram[:midpoint])
+    right_u_base = np.argmax(histogram[midpoint:]) + midpoint
+
+    # Identify (u, v) of all nonzero pixels in the image
+    nonzero_vs, nonzero_us = binary_birdeye_img.nonzero()
+    nonzero_pixels = np.vstack((nonzero_us, nonzero_vs)).transpose()
+
+    # Find pixels for the two lanes
+    # NOTE an alternative method is to cluster the pixels with additional info
+    # from instance segmentation done by LaneNet
+    left_lane_pixels = _find_lane_edge_pixels(nonzero_pixels, int(left_u_base), img_size)
+    right_lane_pixels = _find_lane_edge_pixels(nonzero_pixels, int(right_u_base), img_size)
+    return left_lane_pixels, right_lane_pixels
 
 
 class LaneNetWLineFit:
+    M, M_INV = get_perspective_transform(SRC_DST_MAP)
+
     def __init__(self, config_path: str, weights_path: str,
-                 use_gpu: bool = True, window_size: int = 5) -> None:
+                 use_gpu: bool = True, debug: bool = False, window_size: int = 5) -> None:
         if tf.compat.v1.executing_eagerly():
             raise RuntimeError(
                 "Currently only support running TensorFlow with eager execution disabled."
@@ -16,6 +204,8 @@ class LaneNetWLineFit:
 
         self._cfg = Config(config_path=config_path)
         self._init_lanenet(weights_path, use_gpu)
+        self._cluster = lanenet_postprocess._LaneNetCluster(cfg=self._cfg)
+        self._debug = debug
 
         self.detected = False
         self.left_line = Line(n=window_size)
@@ -42,7 +232,7 @@ class LaneNetWLineFit:
         saver.restore(sess=self._sess, save_path=weights_path)
 
     @staticmethod
-    def _preprocess(img):
+    def _lanenet_preprocess(img):
         image = cv2.resize(img, (512, 256), interpolation=cv2.INTER_LINEAR)
         image = image / 127.5 - 1.0
         return image
@@ -58,45 +248,152 @@ class LaneNetWLineFit:
         output_arr = (input_arr - min_val) * 255.0 / (max_val - min_val)
         return output_arr
 
-    def _inference_net(self, img, original_img):
+    @staticmethod
+    def _lanenet_postprocess(binary_seg_result, instance_seg_result, min_area_threshold=100):
+        # convert binary_seg_result to 8-bit
+        binary_seg_result = np.array(binary_seg_result * 255, dtype=np.uint8)
+        # Use closing morphology. Closing small holes inside the foreground objects
+        morphological_ret = lanenet_postprocess._morphological_process(binary_seg_result)
+
+        # Use connected components to remove too small components
+        connect_components_analysis_ret = lanenet_postprocess._connect_components_analysis(image=morphological_ret)
+
+        labels = connect_components_analysis_ret[1]
+        stats = connect_components_analysis_ret[2]
+        for index, stat in enumerate(stats):
+            if stat[4] <= min_area_threshold:
+                idx = np.where(labels == index)
+                morphological_ret[idx] = 0
+
+        return morphological_ret
+
+    def _lanenet_pipeline(self, src_img):
+        resized_img = self._lanenet_preprocess(src_img)
         binary_seg_image, instance_seg_image = self._sess.run(
             [self.binary_seg_ret, self.instance_seg_ret],
-            feed_dict={self.input_tensor: [img]}
+            feed_dict={self.input_tensor: [resized_img]}
         )
-        binary_seg_result = np.reshape(binary_seg_image[0], (256, 512)).astype(np.float)
+        # Eliminate redundant dimension which has only one entry
+        binary_seg_result = np.squeeze(binary_seg_image)
+        instance_seg_result = np.squeeze(instance_seg_image)
+
+        binary_seg_result = self._lanenet_postprocess(binary_seg_result, instance_seg_result)
 
         for i in range(self._cfg.MODEL.EMBEDDING_FEATS_DIMS):
-            instance_seg_image[0][:, :, i] = self._minmax_scale(instance_seg_image[0][:, :, i])
-        embedding_image = np.array(instance_seg_image[0], np.uint8)
+            instance_seg_result[:, :, i] = self._minmax_scale(instance_seg_result[:, :, i])
+        embedding_image = np.array(instance_seg_result, np.uint8)
 
-        # Edge detection on embedding instance image
-        edges = np.array(cv2.Canny(embedding_image, 100, 200), dtype=np.float)
-        fusion = cv2.addWeighted(binary_seg_result, 1, edges, 1, 0)
+        # Resize back to the same size as source image
+        src_img_size = (src_img.shape[1], src_img.shape[0])
+        binary_seg_result = cv2.resize(binary_seg_result, src_img_size, interpolation=cv2.INTER_LINEAR)
+        instance_seg_result = cv2.resize(instance_seg_result, src_img_size, interpolation=cv2.INTER_LINEAR)
+        return binary_seg_result, instance_seg_result
 
-        # Fusion morp
-        kernel = np.ones((3, 3), np.uint8)
-        # dilation = cv2.dilate(fusion,kernel,iterations = 1)
-        closing = cv2.morphologyEx(fusion, cv2.MORPH_CLOSE, kernel)
-        closing = closing.astype(np.float32)
-        orig = cv2.resize(original_img, (1280, 720), interpolation=cv2.INTER_LINEAR)
-        binary_seg_image = binary_seg_image.astype(np.float32)
-        img = cv2.resize(img, (1280, 720), interpolation=cv2.INTER_LINEAR)
-        # combined detection added for test
-        fit_ret = line_fit_pipeline(closing, orig, self.detected, self.left_line, self.right_line)
+    @classmethod
+    def _birdeye_view(cls, intermediate_img: np.ndarray, src_img_size: Tuple[int, int]):
+        """ Get bird's eye view from input image
 
-        mask_image = fit_ret['combined_img']
-        offset = fit_ret['lane_offset']
-        avg_cur = fit_ret['avg_curve']
-        self.detected = fit_ret['detected']
-        self.left_line = fit_ret['left_line']
-        self.right_line = fit_ret['right_line']
+            Generate warped image in bird view using cv2.warpPerspective()
+        """
+        if (intermediate_img.shape[1], intermediate_img.shape[0]) != src_img_size:
+            raise ValueError("Transformation to bird's eye assumes that the size of the binary image "
+                             "is the same as the source image")
 
-        return mask_image, offset, avg_cur
+        warped_img = cv2.warpPerspective(intermediate_img, cls.M, WARPED_IMG_SIZE,
+                                         flags=cv2.INTER_LINEAR)
+        return warped_img
 
-    def detect(self, img):
-        resized_img = self._preprocess(img)
-        mask_image, offset, curvature = self._inference_net(resized_img, img)
-        return mask_image, offset, curvature
+    def _cluster_lane_chords(self, binary_birdeye_img, instance_birdeye_img):
+        # FIXME This function is not used for now.
+        #  Clustering on the original image size is too slow.
+        #  Clustering on shrunk images did not do well and further requires mapping the pixels to original size.
+        mask, lane_chords = self._cluster.apply_lane_feats_cluster(binary_birdeye_img, instance_birdeye_img)
+
+        # Enlarge back to original size
+        mask = cv2.resize(mask, WARPED_IMG_SIZE, interpolation=cv2.INTER_LINEAR)
+        return mask, lane_chords
+
+    def detect(self, src_img) -> Tuple[float, float, float, np.ndarray]:
+        src_img_size = (src_img.shape[1], src_img.shape[0])
+
+        binary_seg_result, instance_seg_result = self._lanenet_pipeline(src_img)
+
+        binary_birdeye_img = self._birdeye_view(binary_seg_result, src_img_size)
+
+        left_lane_pixels, right_lane_pixels = _find_current_lane_pixels(binary_birdeye_img)
+
+        # Fit a second order polynomial curve to the pixel coordinates for both lanes
+        # Note that v is the x-axis for fitting to follow the right hand rule
+        # TODO fit both lanes to two parallel curves instead of fitting curves separately
+        u_arr, v_arr = left_lane_pixels.T
+        left_line = Polynomial.fit(v_arr, u_arr, deg=1,
+                                   domain=[0, WARPED_IMG_H], window=[0, WARPED_IMG_H])  # type: Polynomial
+        u_arr, v_arr = right_lane_pixels.T
+        right_line = Polynomial.fit(v_arr, u_arr, deg=1,
+                                    domain=[0, WARPED_IMG_H], window=[0, WARPED_IMG_H])  # type: Polynomial
+        center_line = (left_line + right_line) / 2
+
+        # calculate error w.r.t center line first
+        # NOTE coefficients are in the order of u = c[0] + c[1]*v (+ ... + c[n]*v^n)
+        heading_err = np.arctan(-center_line.coef[1])
+        # base_footprint is assumed at (WARPED_IMG_W//2, WARPED_IMG_H) pixel
+        u_diff = WARPED_IMG_W//2 - center_line(WARPED_IMG_H)
+        distance_err = u_diff * np.cos(heading_err) * METER_PER_PIXEL
+
+        # convert error w.r.t the base_footprint of the vehicle (ego view).
+        yaw_err = -heading_err
+        offset = -distance_err
+        curvature = 0.0  # Using line hence the curvature is 0
+
+        if not self._debug:
+            return yaw_err, offset, curvature, binary_birdeye_img
+        # else:  # For DEBUGGING
+        # TODO Check unwarped image is close to binary_seg_result
+        colored_birdeye_img = cv2.cvtColor(binary_birdeye_img, cv2.COLOR_GRAY2RGB)
+
+        # Plot vehicle forward direction
+        cv2.line(colored_birdeye_img,
+                 pt1=(WARPED_IMG_W//2, 0), pt2=(WARPED_IMG_W//2, WARPED_IMG_H),
+                 color=(127, 127, 127), thickness=5)
+
+        # Plot pixels used for line fitting
+        for u, v in left_lane_pixels:
+            colored_birdeye_img[v, u] = np.array([127, 255, 127])
+        for u, v in right_lane_pixels:
+            colored_birdeye_img[v, u] = np.array([255, 127, 127])
+        v_arr = np.arange(WARPED_IMG_H, dtype=np.int32)
+        left_u_arr = left_line(v_arr).astype(np.int32)
+        left_poly_line = np.vstack((left_u_arr, v_arr), ).T
+
+        rospy.loginfo_once("Center line: %s" % str(center_line))
+        center_u_arr = center_line(v_arr).astype(np.int32)
+        center_poly_line = np.vstack((center_u_arr, v_arr), ).T
+
+        right_u_arr = right_line(v_arr).astype(np.int32)
+        right_poly_line = np.vstack((right_u_arr, v_arr)).T
+        cv2.polylines(colored_birdeye_img, [left_poly_line, center_poly_line, right_poly_line], False,
+                      color=(0, 0, 255), thickness=5)
+        colored_unwarped_img = cv2.warpPerspective(colored_birdeye_img, self.M_INV, src_img_size,
+                                                   flags=cv2.INTER_LINEAR)
+        layered_image = cv2.addWeighted(src_img, 1, colored_unwarped_img, 0.5, 0)
+        label_str = 'Heading error: %.1f deg' % np.rad2deg(heading_err)
+        layered_image = cv2.putText(layered_image, label_str, (30, 40), 0, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+        label_str = 'Lane center offset from vehicle: %.1f m' % distance_err
+        layered_image = cv2.putText(layered_image, label_str, (30, 70), 0, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+        # crop margin to aligned the size
+        # base_footprint should map from (WARPED_IMG_W/2, WARPED_IMG_H) to (src_w/2, src_h)
+        src_w, src_h = src_img_size
+        new_w_start = WARPED_IMG_W // 2 - src_w // 2
+        new_h_start = WARPED_IMG_H - src_h
+        cropped_birdeye_img = colored_birdeye_img[new_h_start:new_h_start + src_h,
+                                                  new_w_start:new_w_start + src_w]  # row and column
+
+        # Build final image for debugging
+        debug_image = np.vstack((cropped_birdeye_img, layered_image))
+        return heading_err, distance_err, curvature, debug_image
 
     def close(self) -> None:
         self._sess.close()  # TODO add support for `with` statement using context manager
+
