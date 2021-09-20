ec4ecccdae2f538bda66ae4f50ab4e8882d90bda
Hsieh, Chiao <chsieh16@illinois.edu>
2021-09-20 11:24:38 -0500

    Add debugging info

--- polaris_gem_lane_detect_sim/gem_lanenet/nodes/find_perspective_transform.py
+++ polaris_gem_lane_detect_sim/gem_lanenet/nodes/find_perspective_transform.py
@@ -31,8 +31,13 @@ class TransformFinder:
         self._pub_annotated_img = pub_annotated_img
         self._cam_model = None  # type: Optional[PinholeCameraModel]
         self._cv_bridge = CvBridge()
+        self._src_dst_map = dict()  # type: Dict[Tuple[int, int], Tuple[int, int]]
 
-        print(*self.MARKER_POS_LIST, sep='\n')
+        rospy.logdebug("List of marker positions %s" % self.MARKER_POS_LIST)
+
+    @property
+    def src_dst_map(self) -> Dict[Tuple[int, int], Tuple[int, int]]:
+        return self._src_dst_map
 
     def cam_info_callback(self, msg: CameraInfo) -> None:
         if self._cam_model is None:
@@ -51,6 +56,7 @@ class TransformFinder:
 
             src_pts = self._get_pos_to_src_pts()
             dst_pts = self._get_pos_to_dst_pts()
+            self._src_dst_map = dict(zip(src_pts, dst_pts))
             src_pts_arr = np.float32(src_pts)
             dst_pts_arr = np.float32(dst_pts)
             matrix, mask = cv2.findHomography(src_pts_arr, dst_pts_arr, cv2.RANSAC, 5.0)
@@ -59,17 +65,18 @@ class TransformFinder:
 
             # Mark the positions on original and warped images
             for (u, v) in src_pts:
-                cv2.circle(rectified_cv_img, (int(u), int(v)), radius=5, color=(0, 255, 0), thickness=-1)
+                cv2.circle(rectified_cv_img, (u, v), radius=5, color=(0, 255, 0), thickness=-1)
             for (u, v) in dst_pts:
-                cv2.circle(warped_cv_img, (int(u), int(v)), radius=5, color=(0, 255, 0), thickness=-1)
+                cv2.circle(warped_cv_img, (u, v), radius=5, color=(0, 255, 0), thickness=-1)
 
             # cropped margin to aligned the size
+            # base_footprint should map from (WARPED_IMG_W/2, WARPED_IMG_H) to (src_w/2, src_h)
             src_h, src_w = rectified_cv_img.shape[0:2]
-            dst_h, dst_w = warped_cv_img.shape[0:2]
-            new_h, new_w = (dst_h - src_h) // 2, (dst_w - src_w) // 2
-            warped_cv_img = warped_cv_img[new_h:new_h+src_h, new_w:new_w+src_w]
+            new_w_start, new_w_stop = WARPED_IMG_W//2 - src_w//2, WARPED_IMG_W//2 + src_w//2
+            new_h_start, new_h_stop = WARPED_IMG_H - src_h, WARPED_IMG_H
+            cropped_cv_img = warped_cv_img[new_h_start:new_h_stop, new_w_start:new_w_stop]  # row and column
 
-            annotated_img = np.vstack((rectified_cv_img, warped_cv_img))
+            annotated_img = np.vstack((rectified_cv_img, cropped_cv_img))
             self._pub_annotated_img.publish(self._cv_bridge.cv2_to_imgmsg(annotated_img))
 
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
@@ -129,7 +136,12 @@ def main(argv):
         "image_raw",
         Image, finder.img_callback, queue_size=1)
     try:
-        rospy.spin()
+        rate = rospy.Rate(hz=0.2)
+        while not rospy.is_shutdown():
+            rate.sleep()
+            if finder.src_dst_map:
+                rospy.loginfo("\n%s" % finder.src_dst_map)
+                rospy.logdebug("Pairs:\n%s" % "\n".join(["%s -> %s" % p for p in finder.src_dst_map.items()]))
     except KeyboardInterrupt:
         rospy.loginfo("Shutting down ROS node")
 
