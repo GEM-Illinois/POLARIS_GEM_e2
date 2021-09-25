4840cb1dab45cc24a6fad404a0c7b9d04ba63f0b
Hsieh, Chiao <chsieh16@illinois.edu>
2021-09-25 16:11:39 -0500

    Return all lane edges instead of two edges

--- polaris_gem_lane_detect_sim/gem_lanenet/test/test_lanenet_w_line_fit.py
+++ polaris_gem_lane_detect_sim/gem_lanenet/test/test_lanenet_w_line_fit.py
@@ -25,7 +25,7 @@ class TestLaneNetWLineFit(TestCase):
         self.nnet = LaneNetWLineFit(self.CONFIG_PATH.as_posix(), self.WEIGHTS_PATH.as_posix(), debug=True)
         # Block and show the image.
         # Set to true if you are running tests manually and want to see the plotted images
-        self.plt_show_block = False
+        self.plt_show_block = True
 
     def test_detect(self):
         for img_path in self.IMAGES_DIR.iterdir():
@@ -33,7 +33,28 @@ class TestLaneNetWLineFit(TestCase):
                 print("Image %s is not a file. Skip." % img_path.as_posix())
                 continue
             img = cv2.imread(img_path.as_posix())
-            center_line, annotated_image = self.nnet.detect(img)
+            img = cv2.resize(img, (960, 540), interpolation=cv2.INTER_LINEAR)
+            line_seq, annotated_image = self.nnet.detect(img)
+
+            left_line, right_line = None, None
+            left_line_y_diff, right_line_y_diff = -np.inf, np.inf
+            # Extrapolate the edge line and calculate the difference to base_footprint
+            for edge_line in line_seq:
+                y_diff = edge_line(0.0) - 0.0
+                if left_line_y_diff < y_diff < 0:
+                    left_line = edge_line
+                    left_line_y_diff = y_diff
+                elif 0 < y_diff < right_line_y_diff:
+                    right_line = edge_line
+                    right_line_y_diff = y_diff
+                elif y_diff == 0:
+                    # TODO raise warning
+                    continue
+
+            if left_line is None or right_line is None:
+                # TODO handle error when detected lanes < 2
+                continue
+            center_line = (left_line + right_line) / 2
 
             yaw_err = np.arctan(center_line.convert().coef[1])
             label_str = 'Heading error: %.2f deg' % np.rad2deg(yaw_err)
@@ -53,7 +74,7 @@ class TestLaneNetWLineFit(TestCase):
 
             plt_mask_img = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
             plt.imshow(plt_mask_img)
-            plt.show(block=self.plt_show_block)
+        plt.show(block=self.plt_show_block)
 
     def tearDown(self) -> None:
         self.nnet.close()
