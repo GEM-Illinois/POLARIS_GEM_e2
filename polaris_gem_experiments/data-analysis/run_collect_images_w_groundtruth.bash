bc809902049c78bbefd8a93ac5d6874afb7b9a2d
Hsieh, Chiao <chsieh16@illinois.edu>
2022-02-03 13:49:56 -0600

    Modify the collect image script to specify world name

--- polaris_gem_experiments/data-analysis/run_collect_images_w_groundtruth.bash
+++ polaris_gem_experiments/data-analysis/run_collect_images_w_groundtruth.bash
@@ -3,15 +3,18 @@
 DEVEL_DIR=$HOME/Public/gem_ws/devel
 BAG_DIR=$HOME/Desktop  # Using absolute path is recommended
 DURATION=1h
-TRUTH_FILE=truths_configs/121_truths-evenly_spaced_11x11-pi_12-1.2m.yaml
+TRUTH_FILE=truths_configs/121_truths-evenly_spaced_11x11-pi_12-1.2m.yaml.orig
 
 source ${DEVEL_DIR}/setup.bash
 export ROS_MASTER_URI=http://localhost:61804
 export GAZEBO_MASTER_URI=http://localhost:61824
 
+WORLD_NAME="$(rospack find gem_scenario_runner)/worlds/one_100m_left_curved_road.world"
+
 # Launch ROS nodes Gazebo simulator in background
 roslaunch gem_scenario_runner collect_images_w_truths.launch bag_dir:=${BAG_DIR} \
-    truth_file:=${TRUTH_FILE} gui:=false \
+    truth_file:=${TRUTH_FILE} gui:=true \
+    world_name:=${WORLD_NAME} \
     record:=true duration:=${DURATION} reset_period:=0.25 &
 ROSLAUNCH_PID=$!
 
