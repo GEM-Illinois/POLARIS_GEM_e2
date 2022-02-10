2e94c96a3afbe0e0330e857957a1d751f033aafb
Hsieh, Chiao <chsieh16@illinois.edu>
2022-02-10 15:53:24 -0600

    Move CBMC program analysis scripts to the other repository

--- polaris_gem_experiments/prog-analysis/run-cbmc.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-MAIN_C='lane_centering_stanley/main.c'
-MACROS='-DAPPROX=CBMC -DASSERT_NONINCREASE'
-
-( set -x; cbmc ${MACROS} --no-unwinding-assertions --unwind 6 --trace ${MAIN_C} ) 2>&1 | tee run-cbmc.log
